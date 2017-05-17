d3fd9c466db8004a3d7e036c53f6108bbaa1a6e7
Gao Shang <gsh1989128@126.com>
2017-05-17 11:09:00 +0800

    Initial commit

---
+++ src/com/shnsg/idea/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.shnsg.idea.demo;
+
+/**
+ * Created by dell on 2017/5/17.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("hello world");
+    }
+}
